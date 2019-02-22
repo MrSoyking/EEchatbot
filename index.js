2968b0ccee64ca96f1ea092f0db03f57cb997fdb
Shaun <Shauntanry@gmail.com>
2019-02-22 20:33:43 +0800

    added a running chatbot

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const Telegraf = require('telegraf');
+
+const APIkey = "683546479:AAEaQHVViitwVnhNms-OoYXgCAJLX0xGBAg";
+
+const bot = new Telegraf(APIkey);
+
+bot.help((ctx) => ctx.reply('Hello'));
+
+bot.command("hello", (ctx) => ctx.reply('Hello back'));
+
+bot.launch();
